8d9924acb8c7e883030c81c21c1beaaa9caa395d
abselyam15 <99846458+abselyam15@users.noreply.github.com>
2024-02-08 21:50:28 +0100

    Fix pathes

--- js/task-1.js
+++ js/task-1.js
@@ -1,23 +1,23 @@
-// function isEnoughCapacity (products, containerSize) {
-//     const totalQuantity = Object.values(products).reduce((acc, quantity) => acc + quantity, 0);
+function isEnoughCapacity (products, containerSize) {
+    const totalQuantity = Object.values(products).reduce((acc, quantity) => acc + quantity, 0);
 
-//     return totalQuantity <= containerSize;
-// }
+    return totalQuantity <= containerSize;
+}
 
 
 
-// console.log(
-//     isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-//   ); // true
+console.log(
+    "[TASK 1]: " + isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
   
-//   console.log(
-//     isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-//   ); // false
+  console.log(
+    "[TASK 1]: " + isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
   
-//   console.log(
-//     isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-//   ); // true
+  console.log(
+    "[TASK 1]: " + isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
   
-//   console.log(
-//     isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-//   ); // false
+  console.log(
+    "[TASK 1]: " + isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
--- js/task-3.js
+++ js/task-3.js
@@ -15,10 +15,10 @@ const profile = {
 
 
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+console.log("[TASK 3]: " + profile.getInfo()); // "Jacob has 300 active hours!"
 
 profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
+console.log("[TASK 3]: " + profile.getInfo()); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+console.log("[TASK 3]: " + profile.getInfo()); // "Marco has 320 active hours!"
